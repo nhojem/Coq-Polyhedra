d2994f22603bfe6c5200f78bdd2a92f65fdf6417
Quentin Canu <qcanu17@gmail.com>
2021-05-15 12:27:08 +0200

    Lexicographic feasible basis redefined

--- theories/enumValid.v
+++ theories/enumValid.v
@@ -36,6 +36,7 @@ End Perturbation.
 Section LexiBasis.
 
 Context {n m: nat} (base_p : seq ('rV[rat]_n * 'rV[rat]_m.+1)).
+Hypothesis base_size: size base_p = m.
 
 Definition sat_prebasis (x : 'M[rat]_(n,m.+1)) :=
   all (fun l => sat_ineq l x) base_p.
@@ -44,15 +45,16 @@ Definition eq_prebasis (mas : bitseq) (x : 'M[rat]_(n, m.+1)) :=
   all (fun l => sat_eq l x) (mask mas base_p).
 
 Definition lexi_preaxiom (b : (bitseq * 'M[rat]_(n,m.+1))%type) :=
-  [&& sat_prebasis b.2, card_bitseq b.1 == n & eq_prebasis b.1 b.2].
+  [&& sat_prebasis b.2, card_bitseq b.1 == n,
+    size b.1 == m & eq_prebasis b.1 b.2].
 
 Record lexi_prebasis := Lexi {
   prebase :> (bitseq * 'M[rat]_(n,m.+1))%type;
   preaxiom : lexi_preaxiom prebase
 }.
 
-Coercion lexi_mask (L : lexi_prebasis) : bitseq := L.1.
-Coercion lexi_point (L : lexi_prebasis) : 'M[rat]_(n, m.+1) := L.2.
+Definition lexi_mask (L : lexi_prebasis) : bitseq := L.1.
+Definition lexi_point (L : lexi_prebasis) : 'M[rat]_(n, m.+1) := L.2.
   
 Canonical prelexi_subType := [subType for prebase].
 Definition prelexi_eqMixin := [eqMixin of lexi_prebasis by <:].
@@ -64,21 +66,62 @@ Lemma lexi_preaxiomP (L : lexi_prebasis) :
   lexi_preaxiom L.
 Proof. exact: preaxiom. Qed.
 
-Lemma lexi_sat_prebasis (L : lexi_prebasis) : sat_prebasis L.
-Proof. by case/and3P: (lexi_preaxiomP L). Qed.
+Lemma lexi_sat_prebasis (L : lexi_prebasis) :
+  sat_prebasis (lexi_point L).
+Proof. by case/and4P: (lexi_preaxiomP L). Qed.
 
 Lemma lexi_card (L : lexi_prebasis) :
-  card_bitseq L = n.
-Proof. by apply/eqP; case/and3P: (lexi_preaxiomP L). Qed.
+  card_bitseq (lexi_mask L) = n.
+Proof. by apply/eqP; case/and4P: (lexi_preaxiomP L). Qed.
 
-Lemma lexi_eq_prebasis (L: lexi_prebasis) : eq_prebasis L L.
-Proof. by case/and3P: (lexi_preaxiomP L). Qed.
+Lemma lexi_size (L : lexi_prebasis) :
+  size (lexi_mask L) = m.
+Proof. by apply/eqP; case/and4P : (lexi_preaxiomP L). Qed.
+
+Lemma lexi_eq_prebasis (L: lexi_prebasis) :
+  eq_prebasis (lexi_mask L) (lexi_point L).
+Proof. by case/and4P: (lexi_preaxiomP L). Qed.
+
+Definition lexi_matrix (L : lexi_prebasis) : 'M_n :=
+  \matrix_(i < n) (mask (lexi_mask L) (unzip1 base_p))`_i.
+
+Definition lexi_aff (L : lexi_prebasis) : 'M_(n, m.+1) :=
+  \matrix_(i < n) (mask (lexi_mask L) (unzip2 base_p))`_i.
+
+Lemma lexi_matrix_affP (L : lexi_prebasis) x:
+  reflect
+  ((lexi_matrix L) *m x = (lexi_aff L))
+  (eq_prebasis (lexi_mask L) x).
+Proof.
+apply/(iffP idP).
+- move/allP=> /= h; apply/row_matrixP => i.
+  rewrite row_mul !rowK -!map_mask.
+  set s:= mask _ _ in h *.
+  have sz_s : size s = n by
+  rewrite size_mask ?lexi_size ?base_size -?(lexi_card L).
+  move: (ltn_ord i); rewrite -{2}sz_s=> i_lt.
+  move/eqP: (h s`_i (mem_nth _ i_lt)).
+  congr (_ = _); first congr (_ *m _); exact/esym/nth_map.
+- move/row_matrixP=> h; apply/allP => l /nthP /= /(_ 0) [i i_lt <-].
+  set s := mask _ _ in i_lt *.
+  have sz_s : size s = n by
+  rewrite size_mask ?lexi_size ?base_size -?(lexi_card L).
+  rewrite sz_s in i_lt; move: (h (Ordinal i_lt)).
+  rewrite row_mul !rowK -!map_mask !(nth_map 0) ?sz_s //=.
+  by move/eqP.
+Qed.
 
 
+
+Lemma lexi_solP (L: lexi_prebasis):
+  (lexi_matrix L) *m (lexi_point L) = (lexi_aff L).
+Proof. exact/lexi_matrix_affP/lexi_eq_prebasis. Qed.
+
+ 
 (* ------------------------------------------------------------------- *)
 
 Definition is_basis (L : lexi_prebasis) : bool :=
-  \dim <<(mask L (unzip1 base_p))>>%VS == n.
+  (lexi_matrix L) \in unitmx.
   
 
 Record lexi_basis := LexiB
@@ -96,42 +139,12 @@ Canonical lexi_choiceType := ChoiceType _ lexi_choiceMixin.
 Lemma lexi_basisP (L : lexi_basis) : is_basis L.
 Proof. by case: L. Qed.
 
-Definition matrix_of_seqrV {k : nat} {R : realFieldType}
-  (s : seq 'rV[R]_k) :=
-  \matrix_(i < size s) nth (\row_(j < k) 0) s i.
-
-
-
-
-
-(* Lemma eq_prebasisE (mas : bitseq) (x : 'M[rat]_(n, m.+1)):
-  (eq_prebasis mas x) =
-  (matrix_of_seqrV (mask mas (unzip1 base_p)) *m x == 
-  matrix_of_seqrV (mask mas (unzip2 base_p))). *)
-
 Lemma lexi_pointP (L : lexi_basis) M:
-  eq_prebasis L M -> M = L.
+  eq_prebasis (lexi_mask L) M -> M = (lexi_point L).
 Proof.
-rewrite /eq_prebasis /sat_eq; set s := mask _ _.
-have sz_s: size s = n by admit.
-pose A := \matrix_(i < n) (s`_i).1.
-pose B := \matrix_(i < n) (s`_i).2.
-move=> h; have eqM: A *m M = B.
-- apply/row_matrixP => i; rewrite row_mul !rowK.
-  apply/eqP; move/allP: h => /(_ s`_i); apply.
-  by rewrite mem_nth // sz_s.
-have eqL: A *m L = B.
-- apply/row_matrixP => i; rewrite row_mul !rowK.
-  apply/eqP; move/allP: (lexi_eq_prebasis L).
-  by move/(_ s`_i); apply; rewrite mem_nth // sz_s.
-have: A *m M = A *m L by rewrite eqM eqL.
-move/(congr1 trmx); rewrite !trmx_mul => /row_free_inj.
-move=> {}h; apply/trmx_inj; apply: h => { eqM eqL B }.
-rewrite row_free_unit unitmx_tr -row_free_unit.
-rewrite -row_leq_rank leq_eqVlt; apply/orP; left.
-apply/eqP/esym; have := lexi_basisP L; rewrite /is_basis.
-rewrite -map_mask -mxrank_gen => /eqP eqn; rewrite -{}[RHS]eqn.
-Admitted.
+move : (lexi_solP L) => + /lexi_matrix_affP; move=> <-.
+apply: row_full_inj; rewrite row_full_unit; exact: lexi_basisP.
+Qed.
 
 End LexiBasis.
 

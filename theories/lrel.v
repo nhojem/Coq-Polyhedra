5d5d2b1f80f75a6db590e7659928c7da01110ca7
Xavier Allamigeon <xavier.allamigeon@inria.fr>
2021-06-02 10:37:46 +0200

    Experimenting extension of lrel.

--- theories/lrel.v
+++ theories/lrel.v
@@ -20,14 +20,20 @@ Local Open Scope ring_scope.
 Import GRing.Theory Num.Theory.
 
 (* -------------------------------------------------------------------- *)
-Reserved Notation "'lrel' [ R ]_ n"
-  (at level 8, format "'lrel' [ R ]_ n").
+Reserved Notation "R '.-lrel[' T ]_ n"
+  (at level 8, format "R '.-lrel[' T ]_ n").
 
-Reserved Notation "'lrel'"
+Reserved Notation "'lrel[' R ]_ n"
+  (at level 8, format "'lrel[' R ]_ n").
+
+Reserved Notation "'lrel_' n"
   (at level 8).
 
-Reserved Notation "'base_t' [ R , n ]"
-  (at level 8, format "'base_t' [ R , n ]").
+Reserved Notation "R '.-base_t[' T , n ]"
+  (at level 8, format "R '.-base_t[' T , n ]").
+
+Reserved Notation "'base_t[' R , n ]"
+  (at level 8, format "'base_t[' R , n ]").
 
 Reserved Notation "'base_t'"
   (at level 8).
@@ -37,60 +43,62 @@ Reserved Notation "[< A , b >]"
 
 (* -------------------------------------------------------------------- *)
 Section Base.
-Context {R : Type} (n : nat).
+Context {R T : Type} (n : nat).
 
-Variant lrel_type : predArgType := BaseElt of ('cV[R]_n * R).
+Variant lrel_type : predArgType := LRel of ('cV[R]_n * T).
 
-Coercion lrel_val b := let: BaseElt b := b in b.
+Coercion lrel_val b := let: LRel b := b in b.
 
 Canonical lrel_subType := Eval hnf in [newType for lrel_val].
 End Base.
 
-Notation "'lrel' [ R ]_ n" := (@lrel_type R n).
-Notation "'lrel'" := (lrel[_]_(_)).
-Notation "'base_t' [ R , n ]" := {fset lrel[R]_n}.
+Notation "R '.-lrel['  T ]_ n" := (@lrel_type R T n).
+Notation "'lrel[' R ]_ n" := (R.-lrel[R^o]_n).
+Notation "'lrel_' n" := (lrel[_]_n).
+Notation "R '.-base_t[' T , n ]" := {fset R.-lrel[T]_n}.
+Notation "'base_t[' R , n ]" := (R.-base_t[R,n]).
 Notation "'base_t'" := (base_t[_,_]).
-Notation "[< A , b >]" := (BaseElt (A, b)).
+Notation "[< A , b >]" := (LRel (A, b)).
 
 (* -------------------------------------------------------------------- *)
-Definition lrel_eqMixin (R : eqType) n :=
-  Eval hnf in [eqMixin of lrel[R]_n by <:].
-Canonical lrel_eqType (R : eqType) n:=
-  Eval hnf in EqType lrel[R]_n  (lrel_eqMixin R n).
-Definition lrel_choiceMixin (R : choiceType) n :=
-  [choiceMixin of lrel[R]_n by <:].
-Canonical lrel_choiceType (R : choiceType) n :=
-  Eval hnf in ChoiceType lrel[R]_n (lrel_choiceMixin R n).
-Definition lrel_countMixin (R : countType) n :=
-  [countMixin of lrel[R]_n by <:].
-Canonical lrel_countType (R : countType) n :=
-  Eval hnf in CountType lrel[R]_n (lrel_countMixin R n).
-Canonical lrel_subCountType (R : countType) n :=
-  Eval hnf in [subCountType of lrel[R]_n].
-Definition lrel_finMixin (R : finType) n :=
-  [finMixin of lrel[R]_n by <:].
-Canonical lrel_finType (R : finType) n :=
-  Eval hnf in FinType lrel[R]_n (lrel_finMixin R n).
-Canonical lrel_subFinType (R : finType) n :=
-  Eval hnf in [subFinType of lrel[R]_n].
+Definition lrel_eqMixin (R T : eqType) n :=
+  Eval hnf in [eqMixin of R.-lrel[T]_n by <:].
+Canonical lrel_eqType (R T : eqType) n:=
+  Eval hnf in EqType R.-lrel[T]_n  (lrel_eqMixin R T n).
+Definition lrel_choiceMixin (R T : choiceType) n :=
+  [choiceMixin of R.-lrel[T]_n by <:].
+Canonical lrel_choiceType (R T : choiceType) n :=
+  Eval hnf in ChoiceType R.-lrel[T]_n (lrel_choiceMixin R T n).
+Definition lrel_countMixin (R T : countType) n :=
+  [countMixin of R.-lrel[T]_n by <:].
+Canonical lrel_countType (R T : countType) n :=
+  Eval hnf in CountType R.-lrel[T]_n (lrel_countMixin R T n).
+Canonical lrel_subCountType (R T : countType) n :=
+  Eval hnf in [subCountType of R.-lrel[T]_n].
+Definition lrel_finMixin (R T : finType) n :=
+  [finMixin of R.-lrel[T]_n by <:].
+Canonical lrel_finType (R T : finType) n :=
+  Eval hnf in FinType R.-lrel[T]_n (lrel_finMixin R T n).
+Canonical lrel_subFinType (R T : finType) n :=
+  Eval hnf in [subFinType of R.-lrel[T]_n].
 
 (* -------------------------------------------------------------------- *)
 Section BaseTheory.
-Context (R : Type) (n : nat).
+Context (R T : Type) (n : nat).
 
-Lemma lrelW (P : lrel[R]_n -> Prop) :
+Lemma lrelW (P : R.-lrel[T]_n -> Prop) :
   (forall A b, P [<A, b>]) -> (forall b, P b).
 Proof. by move=> ih [[]]. Qed.
 
-Lemma lrelE (b : lrel[R]_n) : [<b.1, b.2>] = b.
+Lemma lrelE (b : R.-lrel[T]_n) : [<b.1, b.2>] = b.
 Proof. by elim/lrelW: b. Qed.
 End BaseTheory.
 
-Lemma lrel_eqE (R : eqType) n (b1 b2 : lrel[R]_n) :
+Lemma lrel_eqE (R T : eqType) n (b1 b2 : R.-lrel[T]_n) :
   (b1 == b2) = [&& b1.1 == b2.1 & b1.2 == b2.2].
 Proof. by []. Qed.
 
-Lemma lrel_eqP (R : eqType) n (b1 b2 : lrel[R]_n) :
+Lemma lrel_eqP (R T : eqType) n (b1 b2 : R.-lrel[T]_n) :
   reflect (b1.1 = b2.1 /\ b1.2 = b2.2) (b1 == b2).
 Proof.
 rewrite lrel_eqE; apply: (iffP andP).
@@ -137,11 +145,11 @@ End BaseEncodingTheory.
 
 (* -------------------------------------------------------------------- *)
 Section BaseZmod.
-Context {R : zmodType} {n : nat}.
+Context {R T : zmodType} {n : nat}.
 
-Implicit Types (b : lrel[R]_n).
+Implicit Types (b : R.-lrel[T]_n).
 
-Definition lrel0         := [< (0 : 'cV[R]_n), (0 : R) >].
+Definition lrel0         := [< (0 : 'cV[R]_n), (0 : T) >].
 Definition lrel_add b1 b2 := [< b1.1 + b2.1, b1.2 + b2.2 >].
 Definition lrel_opp b     := [< -b.1, -b.2 >].
 
@@ -163,7 +171,7 @@ Let lrel_add0r := let: And4 _ _ h _ := lrel_zmod_mixin in h.
 Let lrel_addNr := let: And4 _ _ _ h := lrel_zmod_mixin in h.
 
 Definition lrel_zmodMixin := ZmodMixin lrel_addA lrel_addC lrel_add0r lrel_addNr.
-Canonical lrel_zmodType := Eval hnf in ZmodType lrel lrel_zmodMixin.
+Canonical lrel_zmodType := Eval hnf in ZmodType R.-lrel[T]_n lrel_zmodMixin.
 
 Lemma lrel_addE b1 b2 : b1 + b2 = [< b1.1 + b2.1, b1.2 + b2.2 >].
 Proof. by []. Qed.
@@ -173,7 +181,7 @@ Proof. by []. Qed.
 End BaseZmod.
 
 (* -------------------------------------------------------------------- *)
-Section BaseEltEncodingZmodMorph.
+Section LRelEncodingZmodMorph.
 Context {R : zmodType} {n : nat}.
 
 Lemma lrel_to_col_is_additive : additive (@lrel_to_col R n).
@@ -183,15 +191,15 @@ by case: splitP'=> j _; rewrite !mxE.
 Qed.
 
 Canonical lrel_to_col_additive := Additive lrel_to_col_is_additive.
-End BaseEltEncodingZmodMorph.
+End LRelEncodingZmodMorph.
 
 (* -------------------------------------------------------------------- *)
 Section BaseLmod.
-Context {R : ringType} {n : nat}.
+Context {R : ringType} {T : lmodType R} {n : nat}.
 
-Implicit Types (b : lrel[R]_n).
+Implicit Types (b : R.-lrel[T]_n).
 
-Definition lrel_scale c b := [< c *: b.1, c * b.2 >].
+Definition lrel_scale c b := [< c *: b.1, c *: b.2 >].
 
 Lemma lrel_lmod_mixin :
   [/\ forall c1 c2 b, lrel_scale c1 (lrel_scale c2 b) = lrel_scale (c1 * c2) b
@@ -199,10 +207,10 @@ Lemma lrel_lmod_mixin :
     , right_distributive lrel_scale +%R
     & forall b, {morph lrel_scale^~ b : x y / x + y}].
 Proof. split.
-+ by move=> c1 c2 b; rewrite /lrel_scale scalerA mulrA.
-+ by move=> b; rewrite /lrel_scale scale1r mul1r lrelE.
-+ by move=> c b1 b2; rewrite /lrel_scale scalerDr !lrel_addE mulrDr.
-+ by move=> b c1 c2; rewrite /lrel_scale lrel_addE scalerDl mulrDl.
++ by move=> c1 c2 b; rewrite /lrel_scale !scalerA.
++ by move=> b; rewrite /lrel_scale !scale1r lrelE.
++ by move=> c b1 b2; rewrite /lrel_scale !scalerDr !lrel_addE.
++ by move=> b c1 c2; rewrite /lrel_scale lrel_addE !scalerDl.
 Qed.
 
 Let lrel_scaleA  := let: And4 h _ _ _ := lrel_lmod_mixin in h.
@@ -211,14 +219,22 @@ Let lrel_scaleDr := let: And4 _ _ h _ := lrel_lmod_mixin in h.
 Let lrel_scaleDl := let: And4 _ _ _ h := lrel_lmod_mixin in h.
 
 Definition lrel_lmodMixin := LmodMixin lrel_scaleA lrel_scale1 lrel_scaleDr lrel_scaleDl.
-Canonical lrel_lmodType := Eval hnf in LmodType R lrel lrel_lmodMixin.
+Canonical lrel_lmodType := Eval hnf in LmodType R R.-lrel[T]_n lrel_lmodMixin.
 
-Lemma lrel_scaleE c b : c *: b = [< c *: b.1, c * b.2 >].
+Lemma lrel_scaleE c b : c *: b = [< c *: b.1, c *: b.2 >].
 Proof. by []. Qed.
 End BaseLmod.
 
+Section Test.
+Variable (R : ringType) (n : nat) (e : lrel[R]_n) (x : R).
+
+Goal  (x *: e) = 0.
+rewrite lrel_scaleE -mulr_algl.
+Abort.
+End Test.
+
 (* -------------------------------------------------------------------- *)
-Section BaseEltEncodingLmodMorph.
+Section LRelEncodingLmodMorph.
 Context {R : ringType} {n : nat}.
 
 Lemma lrel_to_col_is_scalable : scalable (@lrel_to_col R n).
@@ -228,7 +244,7 @@ by case: splitP'=> j _; rewrite !mxE.
 Qed.
 
 Canonical lrel_to_col_scalable := AddLinear lrel_to_col_is_scalable.
-End BaseEltEncodingLmodMorph.
+End LRelEncodingLmodMorph.
 
 (* -------------------------------------------------------------------- *)
 Section BaseMorph.
